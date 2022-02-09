e8c0f8239dbb41bf70742b6b68c004fe6edb4941
quentinece17 <q.appelghem@icloud.com>
2022-02-09 17:23:42 +0100

    structure

---
+++ front-end/src/Theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#44566c',
+    },
+    primary: {
+      main: '#44566c',
+    },
+    secondary: {
+      main: '#2f435e',
+    },
+  },
+});
+
+export default theme;
